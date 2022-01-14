d6c734b84b5a353ff575a106e24d99c727cb1ae5
unknown <aly.delvalle.9@gmail.com>
2022-01-14 22:07:13 +0800

    add proxied url and display more than 1 comic

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 // display comics, add next, previous, and random functionality
 
 (function getComic(){
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com';
-    // const proxyUrl = 'https://cors.bridged.cc';
 // nav buttons
     const prevButton = document.querySelector('#prev-btn');
     const nextButton = document.querySelector('#next-btn');
     const randomButton = document.querySelector('#random-btn');
+// display number dropdown
+    const displayStrips = document.getElementById('numOfComics')
 // content
     const content = document.querySelector('.comic');
     const date = document.querySelector('.date');
@@ -18,52 +18,81 @@
     let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
 
     const proxiedRequest = (url) =>
-        fetch(`${proxyUrl}/${url}`, {
-        headers: {
-            'X-Requested-With': 'wololo',
-        },
+        fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(url)}`)
+        .then(response => {
+            if (response.ok) return response.json()
+            throw new Error('Network response was not ok.')
         })
-        .then(resp => resp.json())
         .then(res => {
-        // display comic and details
-            const image = `<img src="${res.img}" alt="${res.alt}"></img>`;
-            content.innerHTML = image;
-            const imgDate = `<p>${months[res.month-1]} ${res.day} ${res.year}</p>`;
-            date.innerHTML = imgDate;
-            const imgTitle = `<p>${res.title}</p>`;
-            title.innerHTML = imgTitle;
-            console.log(imgTitle)
+            let displayNum = res.num;
+            let next = res.num - 1;
+            console.log(displayNum);
+            console.log(next);
 
-        // set values for next, previous, and random buttons
-            console.log(res.num, typeof res.num)
-            prevPageNum = res.num-1
-            nextPageNum = res.num+1
-            randomPageNum = Math.floor((Math.random() * res.num) + 1);
-        }
-            )
-        .catch(error => console.error(`oh no --> `, error))
+            const comicOrig = document.querySelector('.comicO')
+            const comicA = document.querySelector('.comicA')
+
+            const image = `<img src="${res.img}" alt="${res.alt}"></img>`;
+            comicOrig.innerHTML = image;
+
+            let newUrl = `https://xkcd.com/${next}/info.0.json`;
+
+            fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(newUrl)}`).then(response => {
+                if (response.ok) return response.json()
+                throw new Error('Network response was not ok.')
+            })
+            .then((resTwo => {
+                console.log("second comic: " + resTwo.num)
+
+                const image = `<img src="${resTwo.img}" alt="${resTwo.alt}"></img>`;
+                comicA.innerHTML = image;
+            }))
+         })
+
+    //         const image = `<img src="${res.img}" alt="${res.alt}"></img>`;
+    //         content.innerHTML = image;
+    //         const imgDate = `<p>${months[res.month-1]} ${res.day} ${res.year}</p>`;
+    //         date.innerHTML = imgDate;
+    //         const imgTitle = `<p>${res.title}</p>`;
+    //         title.innerHTML = imgTitle;
+    //         console.log(imgTitle)
+
+    //     // set values for next, previous, and random buttons
+    //         setPageNumbers(res.num)
+    //         randomPageNum = Math.floor((Math.random() * res.num) + 1);
+    //     })
+    //     .catch(error => console.error(`oh no --> `, error))
+
+
+    // const setPageNumbers = (current) => {
+    //     console.log(current, typeof current)
+    //     prevPageNum = current-1
+    //     nextPageNum = current+1
+    // }
 
     const proxiedGet = url => proxiedRequest(url)
 
     proxiedGet('https://xkcd.com/info.0.json')
 
-    function prevComic(){
-        // console.log(prevPageNum)
-        proxiedGet(`https://xkcd.com/${prevPageNum}/info.0.json`)
-    }
+    // function prevComic(){
+    //     // console.log(prevPageNum)
+    //     proxiedGet(`https://xkcd.com/${prevPageNum}/info.0.json`)
+    // }
 
-    function nextComic(){
-        // console.log(nextPageNum)
-        proxiedGet(`https://xkcd.com/${nextPageNum}/info.0.json`)
-    }
+    // function nextComic(){
+    //     // console.log(nextPageNum)
+    //     proxiedGet(`https://xkcd.com/${nextPageNum}/info.0.json`)
+    // }
 
-    function randomComic(){
-        // console.log(randomPageNum)
-        proxiedGet(`https://xkcd.com/${randomPageNum}/info.0.json`)
-    }
+    // function randomComic(){
+    //     // console.log(randomPageNum)
+    //     proxiedGet(`https://xkcd.com/${randomPageNum}/info.0.json`)
+    // }
 
-    prevButton.addEventListener('click', prevComic)
-    nextButton.addEventListener('click', nextComic)
-    randomButton.addEventListener('click', randomComic)
+    // prevButton.addEventListener('click', prevComic)
+    // nextButton.addEventListener('click', nextComic)
+    // randomButton.addEventListener('click', randomComic)
+
+    // displayStrips.addEventListener('change', () => proxiedGet('https://xkcd.com/info.0.json'))
 
 })()
